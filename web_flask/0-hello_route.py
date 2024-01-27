ec879b89bea8d3097b9f14f3ea7917f908bd0c09
Matrix30 <Matrixboy30@gmail.com>
2024-01-27 17:29:08 +0100

    finish task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+scripts that starts a flask web application
+with the route / that returns "Hello HBNB"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Method to run when / route is entered"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
