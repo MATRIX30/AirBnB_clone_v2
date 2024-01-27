02a4a99197757b1136024809a57d3b54c8e4a00f
Matrix30 <Matrixboy30@gmail.com>
2024-01-27 18:31:34 +0100

    finish route with defualt value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+scripts that starts a flask web application
+with the route / that returns "Hello HBNB"
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Method to run when / route is entered"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Method to run when /hbnb route is entered"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c_is_fun(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/<string:text>",  strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+def python_is_cool(text):
+    return f"Python  + {escape(text.replace('_', ' '))}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
