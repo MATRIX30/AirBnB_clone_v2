c4fdca5efef4b7b3b78b74df5273282efb9ca109
Matrix30 <Matrixboy30@gmail.com>
2024-01-31 23:24:32 +0100

    finish task 8 list states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
 """
-from models import storage
-from flask import Flask
-from flask import render_template
+python script to start a flask app with the /states_list route
+returning all the states stored in storage (file/db storage)
+and rendered using jinja template
+"""
 
+from flask import Flask, render_template
+from models import storage, State
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
+@app.route("/states_list", strict_slashes=False)
+def states_list():
     """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    This is the main function that handles the /states_list route
+    """
+    state_list = storage.all(State)
+    print(state_list)
+    return render_template("8-cities_by_states.html", states=state_list)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def close_storage(error=None):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000, debug=True)
